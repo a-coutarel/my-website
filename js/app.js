01ec06afd9a2523f481f00554ac724dd08954645
Allan Coutarel <allancoutarel@orange.fr>
2022-10-02 23:12:41 +0200

    smooth scroll test in comments

--- js/app.js
+++ js/app.js
@@ -86,3 +86,25 @@ window.addEventListener("load", () => {
     });
 
 });
+
+/*
+let panels = gsap.utils.toArray("section"),
+    scrollTween;
+
+function goToSection(i) {
+  scrollTween = gsap.to(window, {
+    scrollTo: {y: i * innerHeight, autoKill: false},
+    duration: 1,
+    onComplete: () => scrollTween = null,
+    overwrite: true
+  });
+}
+
+panels.forEach((panel, i) => {
+  ScrollTrigger.create({
+    trigger: panel,
+    start: "top bottom",
+    end: "+=200%",
+    onToggle: self => self.isActive && !scrollTween && goToSection(i)
+  });
+})*/
