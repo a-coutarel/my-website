23c0b3a4f6e92b22de45951c132c4f3391dfc131
Allan Coutarel <allancoutarel@orange.fr>
2022-10-05 00:04:32 +0200

    responsive for mobile

--- js/app.js
+++ js/app.js
@@ -1,16 +1,13 @@
 gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
 
-const titreSpansH1 = document.querySelectorAll('h1 span');
-const titreSpansH2 = document.querySelectorAll('h2 span');
-const btns = document.querySelectorAll('.btn-first');
-const logo = document.querySelector('.logo');
-const logos = document.querySelectorAll('.bulle'); 
-const l1 = document.querySelector('.l1');
-const l2 = document.querySelector('.l2');
-
 
 window.addEventListener("load", () => {
+
     $("preloader").delay(400).fadeOut('slow');
+
+
+
+
 
     document.querySelector('.b1').addEventListener("click", () => {
         gsap.to(window, {duration: 1, scrollTo: {y: "#discover"}, ease: "power2"});
@@ -24,6 +21,18 @@ window.addEventListener("load", () => {
         gsap.to(window, {duration: 1.5, scrollTo: {y: "#projects"}, ease: "power2"});
     });
 
+
+
+
+    
+    const titreSpansH1 = document.querySelectorAll('h1 span');
+    const titreSpansH2 = document.querySelectorAll('h2 span');
+    const btns = document.querySelectorAll('.btn-first');
+    const pp = document.querySelector('.pp');
+    const logos = document.querySelectorAll('.bulle'); 
+    const l1 = document.querySelector('.l1');
+    const l2 = document.querySelector('.l2');
+
     let TL = gsap.timeline({paused: true});
 
     TL.staggerFrom(titreSpansH1, 1.2, {opacity: 0, ease: "power2.but"}, 0.3);
@@ -31,18 +40,32 @@ window.addEventListener("load", () => {
     TL.staggerFrom(btns, 1.2, {opacity: 0, ease: "power2.but"}, 0.3, '-=1.5');
     TL.from(l1, 2.5, {width: 0, ease: "power2.but"}, '');
     TL.from(l2, 2.5, {width: 0, ease: "power2.but"}, '');
-    TL.from(logo, 1.5, {left: -100, rotate: -360, ease: "power2.but"}, '');
-    TL.staggerFrom(logos, 1, {right: -200, ease: "power2.but"}, 0.3, '-=1.2');
+    TL.from(pp, 3, {left: -100 +"vw", opacity: 0, rotate: -360, ease: "power2.inOut"}, '-=3.5');
+    TL.staggerFrom(logos, 2, {right: -100 +"vw", ease: "circ.out"}, 0.3, '-=1.6');
     
     setTimeout( () => {TL.play()}, 600);
 
-    
+    $(window).resize(function() {
+        l1.style.width = "25vw";
+        l2.style.width = "18vw";
+    });
+
+
+
+
+
     gsap.to(".discover-title", {scrollTrigger: {trigger: ".discover-title", start: "top bottom", toggleActions: "restart pause resume pause"}, opacity: 1, duration: 1.8, ease: "power2"});
     gsap.to(".discover-profile", {scrollTrigger: {trigger: ".discover-profile", start: "top bottom", toggleActions: "restart pause resume pause"}, opacity: 1, duration: 2, ease: "power2"});
     
     
+
+
+
     gsap.to(".education-title", {scrollTrigger: {trigger: ".education-title", toggleActions: "restart pause resume pause", start: "top bottom", end: "bottom center", scrub: 1}, left:"50%", xPercent:-50, ease: "power2"});
     gsap.to(".education-list", {scrollTrigger: {trigger: ".education-list", toggleActions: "restart pause resume pause", start: "top bottom", end: "top center", scrub: 1}, right:"50%", xPercent:50, ease: "power2"});
+
+
+
 
 
     gsap.to(".projects", {scrollTrigger: {trigger: ".projects", start:"200px bottom", end: "-200px top", toggleActions: "restart pause resume pause", scrub: 3}, opacity: 1, ease: "power2"});
@@ -64,14 +87,17 @@ window.addEventListener("load", () => {
     });
 
     
+
+
+    
     gsap.utils.toArray("section").forEach((panel, i) => {
         ScrollTrigger.create({
-          trigger: panel,
-          start: "top top", 
-          pin: true, 
-          pinSpacing: false 
+            trigger: panel,
+            start: "top top", 
+            pin: true, 
+            pinSpacing: false 
         });
-      });
+        });
 
     //gsap.to(".home", {scrollTrigger: {trigger: ".home", start: "top top", pin: true, pinSpacing: false}});
     //gsap.to(".discover", {scrollTrigger: {trigger: ".discover", start: "top top", pin: true, pinSpacing: false}});
@@ -79,13 +105,13 @@ window.addEventListener("load", () => {
     //gsap.to(".projects", {scrollTrigger: {trigger: ".projects", start: "top top", pin: true, pinSpacing: false}});
     //gsap.to("footer", {scrollTrigger: {trigger: "footer", start: "top top", pin: true, pinSpacing: false}});
       
-      ScrollTrigger.create({
-        snap: {
-            snapTo: 1 / (4 - 1),
-            duration: 1,
-            delay: 0,
-            ease: "circ.inOut"
-          }
-      });
+    ScrollTrigger.create({
+    snap: {
+        snapTo: 1 / (4 - 1),
+        duration: 1.2,
+        delay: 0,
+        ease: "circ.Out"
+        }
+    });
 
 });
