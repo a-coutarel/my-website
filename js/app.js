a4b57701ef621c9737f05b45c23523a7a2e268c3
Allan Coutarel <allancoutarel@orange.fr>
2022-10-05 17:07:02 +0200

    another try

--- js/app.js
+++ js/app.js
@@ -115,3 +115,10 @@ window.addEventListener("load", () => {
     });
 
 });
+
+const appHeight = () => {
+const vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+window.addEventListener('resize', appHeight)
+appHeight()
