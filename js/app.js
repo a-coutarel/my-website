d718309934b78e1305f5abae1b37f2185cab4b2a
Allan Coutarel <allancoutarel@orange.fr>
2022-10-05 17:20:42 +0200

    padding bottom to home container for mobile

--- js/app.js
+++ js/app.js
@@ -115,10 +115,3 @@ window.addEventListener("load", () => {
     });
 
 });
-
-const appHeight = () => {
-const vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-window.addEventListener('resize', appHeight)
-appHeight()
